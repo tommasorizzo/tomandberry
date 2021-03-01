ebdbf04a3d0e57ddfaa2432e61aa0ecbf99a9242
tommasorizzo <tommasorizzo@gmail.com>
2021-03-01 13:41:51 +0100

    autoprefixer

--- js/countdown.js
+++ js/countdown.js
@@ -3,7 +3,6 @@ var countDownDate = new Date("Jul 4, 2021 16:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
-
   // Get today's date and time
   var now = new Date().getTime();
 
@@ -16,27 +15,19 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  if (days == 1)
-    var dd = '<p>giorno</p>';
-  else
-    var dd = '<p>giorni</p>';
-  if (hours == 1)
-    var hh = '<p>ora</p>';
-  else
-    var hh = "<p>ore</p>";
-  if (minutes == 1)
-    var mm = '<p>minuto</p>';
-  else
-    var mm = '<p>minuti</p>';
-  if (seconds == 1)
-    var ss = '<p>secondo</p>';
-  else
-    var ss = '<p>secondi</p>';
+  if (days == 1) var dd = "<p>giorno</p>";
+  else var dd = "<p>giorni</p>";
+  if (hours == 1) var hh = "<p>ora</p>";
+  else var hh = "<p>ore</p>";
+  if (minutes == 1) var mm = "<p>minuto</p>";
+  else var mm = "<p>minuti</p>";
+  if (seconds == 1) var ss = "<p>secondo</p>";
+  else var ss = "<p>secondi</p>";
   // Output the result
   document.getElementById("countdown-days").innerHTML = days + dd;
   document.getElementById("countdown-hours").innerHTML = hours + hh;
   document.getElementById("countdown-minutes").innerHTML = minutes + mm;
-  document.getElementById("countdown-seconds").innerHTML = seconds + ss;
+  // document.getElementById("countdown-seconds").innerHTML = seconds + ss;
 
   // If the count down is over, write some text
   if (distance < 0) {
@@ -44,7 +35,7 @@ var x = setInterval(function () {
     document.getElementById("countdown-days").innerHTML = 0;
     document.getElementById("countdown-hours").innerHTML = 0;
     document.getElementById("countdown-minutes").innerHTML = 0;
-    document.getElementById("countdown-seconds").innerHTML = 0;
-    document.getElementsByClassName("cd-item").style.visibility = 'hidden';
+    // document.getElementById("countdown-seconds").innerHTML = 0;
+    // document.getElementsByClassName("cd-item").style.visibility = 'hidden';
   }
 }, 1000);
